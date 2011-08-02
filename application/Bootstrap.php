aec844f8ef81796c645d0c7797b302befd37c6e5
BeNiGoNe <benjamin_volle@hotmail.fr>
2011-08-02 17:19:23 +0200

    Pose d'un template

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,8 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 	protected function _initRouter() {
-
+		$router = Zend_Controller_Front::getInstance()->getRouter();		
+		$routeConfig = new Zend_Config_Ini(CONFIG_PATH . DS . 'routes.ini', 'production');
+		$router->addConfig($routeConfig, 'routes');
 	}
 }
